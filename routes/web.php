32445efd4aa3ec0844a6873f1b60dfab1bce968d
Ohyeaa <r.t.h.lentze@gmail.com>
2025-02-11 16:03:30 +0100

    Modified env.example & started with routes\web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/items', function () {
+
+})->name('items.index');
+
+
+Route::get('/items/create', function () {
+
+})->name('items.create');
+
+
+Route::post('/items', function () {
+
+})->name('items.store');
+
+
+Route::get('/items/{id}', function () {
+    
+})->name('items.show');
+
+
